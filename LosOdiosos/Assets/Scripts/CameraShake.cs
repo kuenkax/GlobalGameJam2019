e37a0df92fed2b11c12a2290c968b5012581159b
fabiansuarez59 <djdrake8@gmail.com>
2019-01-26 17:47:39 +0100

    fabian

--- LosOdiosos/Assets/Scripts/CameraShake.cs
+++ LosOdiosos/Assets/Scripts/CameraShake.cs
@@ -7,37 +7,52 @@ public class CameraShake : MonoBehaviour
     public float radio;
     Vector3 posicionInicial;
     public float velocidad;
-    
+    Vector3 posicion;
     // Start is called before the first frame update
     void Start()
     {
+       
         posicionInicial = transform.position;
-        Shake(60);
+        posicion = posicionInicial;
+        StartCoroutine(Shake(5));
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+     
+    }
+
+    public float tiempoPasado = 0;
+
+    public IEnumerator Shake(float time)
+    {
+        while (true)
+        {
+            tiempoPasado += Time.deltaTime;
+            if(tiempoPasado >= time)
+            {
+                break;
+            }
+                if (Mathf.Round(transform.position.x) == Mathf.Round(posicion.x) && Mathf.Round(transform.position.z) == Mathf.Round(posicion.z))
+                {
+                    posicion = new Vector3(Random.Range(posicionInicial.x - radio, posicionInicial.x + radio), transform.position.y
+            , (Random.Range(posicionInicial.z - radio, posicionInicial.z + radio)));
+                    print(posicion);
+                }
+                transform.position = Vector3.MoveTowards(transform.position, posicion, velocidad);
+            
+
+            yield return null; 
+        }
+
+
+
 
     }
-    Vector3 posicion;
-    public void Shake(float time)
-    {
-        
-       if (Time.time < time)
-        {
-        
-            if(new vecto == posicion)
-            {
-                 posicion = new Vector3(Random.Range(posicionInicial.x - radio, posicionInicial.x + radio),
-          (Random.Range(posicionInicial.x - radio, posicionInicial.x + radio)), transform.position.z);
-            }
-            else
-            {
-                transform.position = Vector3.MoveTowards(transform.position, posicion, velocidad);
-            }
-        }
     }
-}
+
 
 
