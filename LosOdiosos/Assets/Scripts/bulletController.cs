d712873dece62133c832d5f52eebe1e77231b72d
KuenKaStudios <acuencalop@gmail.com>
2019-01-26 11:42:12 +0100

    Bala y Mira a camara
    Scripts y mi escena

---
+++ LosOdiosos/Assets/Scripts/bulletController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+    public float velocity;
+    public float damage;
+    public float lifeTime;
+
+    public 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(this.gameObject, lifeTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the bullet
+        transform.Translate(Vector3.forward * velocity * Time.deltaTime);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ LosOdiosos/Assets/Scripts/lookCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookCamera : MonoBehaviour
+{
+    public Camera m_Camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetVector = this.transform.position - m_Camera.transform.position;
+        transform.rotation = Quaternion.LookRotation(targetVector, m_Camera.transform.rotation * Vector3.up);
+        
+    }
+
+    void RotationEngine() {
+        Vector3 cameraVector = Camera.main.transform.position - transform.position;
+        float newYangle = Mathf.Atan2(cameraVector.z, cameraVector.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, -1 * newYangle, 0);
+
+    }
+}
